404cfd9edb69300d919c048c2e41d52cb27d5708
Дмитрий Абдрахманов <565963@gmail.com>
2016-10-12 17:29:40 +0300

    Dedicated Functions
    (Array Sort) выделенны функции работы с окном и в консоли

--- Software Development/ArraySort/ArraySort/Program.cs
+++ Software Development/ArraySort/ArraySort/Program.cs
@@ -11,13 +11,45 @@ namespace ArraySort
     {
         static void Main(string[] args)
         {   
-            int a = 1;
-            if (a == 1)
+            // Пользовательский интерфейс консоли для начала работы
+            Console.WriteLine("|-------------------------------------------------|");
+            Console.WriteLine("|------Программа сортировки массивов данных-------|");
+            Console.WriteLine("|---------сортирует вводимые целые числа----------|");
+            Console.WriteLine("|может работать в консоли или в графическом режиме|");
+            Console.WriteLine("|-------------------------------------------------|");
+            Console.WriteLine(" выберите режим:");
+            Console.WriteLine(" 1 - создать окно");
+            Console.WriteLine(" иное - продолжать в консоли");
+            Console.Write(" ваш выбор: ");
+            
+            // обработка начальной ситуации, проверка необходимости открытия окна
+            int MC;
+            string ModeChoose = System.Console.ReadLine();
+            MC = int.Parse(ModeChoose);
+            
+            // формирование окна GUI? 
+            if (MC == 1)
             {
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new MainWindow());
+                InitWindow();
+                // После выполнение задачи в GUI - закрытие консоли
+                return;
             }
+
+            // Или продолжение работы в консоли
+            ConsoleSolution();
+        }
+
+        static void InitWindow()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainWindow());
+        }
+
+        static void ConsoleSolution()
+        {
+            Console.Clear();
+            Console.WriteLine("Начинаем");
         }
     }
 }
