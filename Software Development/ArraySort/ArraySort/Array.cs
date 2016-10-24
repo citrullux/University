ef05ec3e2959145b95011410bc0790bff8e3bccd
Дмитрий Абдрахманов <565963@gmail.com>
2016-10-24 09:49:19 +0300

    Re-added class

---
+++ Software Development/ArraySort/ArraySort/Array.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraySort
+{
+    class Array
+    {
+        int size;
+        int[] array;
+
+        public static void arrayCreate()
+        {
+            
+        }
+
+        public static void arraySort()
+        {
+
+        }
+
+        public static void arrayOutput()
+        {
+
+        }
+    }
+}
